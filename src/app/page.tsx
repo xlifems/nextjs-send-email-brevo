af9026526f79e1e959dd0b798820fe04042d848d
Felix Masa <felix.masa@servinformacion.com>
2025-02-09 12:59:31 -0500

    Add initial project setup with Tailwind CSS, Next.js, and essential configurations

---
+++ src/app/page.tsx
@@ -0,0 +1,46 @@
+export default function HomePage() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <form action="/api/contact" method="post" className="flex flex-col gap-4">
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Enter your name"
+            required
+            className="bg-slate-400 block p-2 rounded-md w-full placeholder:text-slate-500"
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Enter your email"
+            required
+            className="bg-slate-400 block p-2 rounded-md w-full placeholder:text-slate-500"
+          />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea
+            id="message"
+            name="message"
+            placeholder="Enter your message"
+            required
+            className="bg-slate-400 block p-2 rounded-md w-full placeholder:text-slate-500"
+          ></textarea>
+        </div>
+        <button
+          type="submit"
+          className="bg-teal-600 hover:bg-teal-800 text-white p-2 rounded-md"
+        >
+          Send Email
+        </button>
+      </form>
+    </div>
+  );
+}
